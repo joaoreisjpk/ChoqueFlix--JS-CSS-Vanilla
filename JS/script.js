87cc149a936f1e8a0898ef9d30c8067d02199611
João Reis <joaoreisjpk@gmail.com>
2021-08-24 17:37:13 -0300

    automatizando a lista de filmes - joao Reis

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
-const url = 'https://api.themoviedb.org/3/movie/400?api_key=ca19804bba1e445e3db2ec8fbecda738';
+const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=ca19804bba1e445e3db2ec8fbecda738';
 const urlImg = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
 
+const getDiv = document.getElementById('film-list')
 const getIMG = document.getElementById('imgtest');
 const getTitle = document.getElementById('titleTest');
 
@@ -9,7 +10,7 @@ function createElement(element, className, content) {
   el.className = className;
   el.innerText = content;
   return el;
-}
+};
 
 function createImg(className, source, alt) {
   const img = document.createElement('img');
@@ -17,27 +18,25 @@ function createImg(className, source, alt) {
   img.src = source;
   img.alt = alt;
   return img;
-}
-
-const carregando = async () => {
-  const section = document.createElement('section');
-  section.innerHTML = 'Loading...';
-  section.className = 'loading';
-  document.querySelector('#film-list').appendChild(section);
-  };
+};
 
 const listaDeFilmes =  async () => {
   const lista = await fetch(url);
   const listaJson = await lista.json();
-  console.log(listaJson);
-  const thumbnail = urlImg+listaJson.poster_path;
-  const title = listaJson.original_title;
-  const note = listaJson.vote_average;
-  getIMG.src = thumbnail;
-  getTitle.innerText = `${title} ${note}`;
-  createElement('div', 'test', listaJson.overview)
-  createImg('imgTest',)
+  listaJson.results.forEach(({ title, vote_average, poster_path, overview}) => {
+    const thumbnail = urlImg + poster_path;
+    const title2 = title;
+    const note = vote_average;
+    const img = createImg('imgTest', thumbnail, overview)
+    const div = createElement('div', 'filme', '')
+    const h2 = createElement('h2', 'filmTitle', `${title2} ${note}`)
+    div.appendChild(img);
+    div.appendChild(h2);
+    getDiv.appendChild(div);
+  });
+
 };
+
 
 window.onload = () => {
   listaDeFilmes();
