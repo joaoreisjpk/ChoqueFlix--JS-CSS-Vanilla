360a28db9b878c5fa4aad80295cfe732424c6063
João Reis <joaoreisjpk@gmail.com>
2021-08-24 15:56:12 -0300

    fetch att

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
-yeah
+const listaDeFilmes =  async () => {
+  const lista = await fetch('https://api.themoviedb.org/3/movie/550?api_key=ca19804bba1e445e3db2ec8fbecda738');
+  const listaJson = await lista.json();
+  console.log(listaJson);
+};
+
+window.onload = () => {
+  listaDeFilmes();
+};
