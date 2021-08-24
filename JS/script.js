733550d2c0eefe92721583f6de0592d1a56510a0
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-08-24 16:23:47 -0300

    Undefined Commit

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 const listaDeFilmes =  async () => {
-  const lista = await fetch('https://api.themoviedb.org/3/movie/550?api_key=ca19804bba1e445e3db2ec8fbecda738');
+  const lista = await fetch('https://api.themoviedb.org/3/movie/550/images?api_key=ca19804bba1e445e3db2ec8fbecda738');
   const listaJson = await lista.json();
   console.log(listaJson);
 };
