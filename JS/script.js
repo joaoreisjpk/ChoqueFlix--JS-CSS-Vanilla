3df5b1e1295af4d9a0be88c971d8d61dd7375701
Gabriel Takaki <gabriel.f.takaki@gmail.com>
2021-08-24 16:58:02 -0300

    adicionando loading

--- JS/script.js
+++ JS/script.js
@@ -4,9 +4,19 @@ const urlImg = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
 const getIMG = document.getElementById('imgtest');
 const getTitle = document.getElementById('titleTest')
 
+const carregando = async () => {
+  const section = document.createElement('section');
+  section.innerHTML = 'Loading...';
+  section.className = 'loading';
+  document.querySelector('#test').appendChild(section);
+};
+// adiciona uma frase 'loading' enquanto se faz a requisição da API
+
 const listaDeFilmes =  async () => {
+  carregando();
   const lista = await fetch(url);
   const listaJson = await lista.json();
+  document.querySelector('.loading').remove();
   console.log(listaJson);
   const thumbnail = urlImg+listaJson.poster_path;
   const title = listaJson.original_title;
@@ -14,6 +24,7 @@ const listaDeFilmes =  async () => {
   getIMG.src = thumbnail;
   getTitle.innerText = `${title} ${note}`;
 };
+// faz a requisição da API e transforma em objeto Json
 
 window.onload = () => {
   listaDeFilmes();
